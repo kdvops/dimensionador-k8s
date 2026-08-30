73524a472ad0fc6b79e462aae2475d6ae014d8bf
kdvops <kelvin.ag89@gmail.com>
2026-08-30 15:05:52 -0400

    infra: add Terraform deployment for Azure Container Apps

---
+++ terraform/outputs.tf
@@ -0,0 +1,20 @@
+output "container_app_id" {
+  value = azurerm_container_app.this.id
+}
+
+output "container_app_fqdn" {
+  value = azurerm_container_app.this.ingress[0].fqdn
+}
+
+output "container_app_url" {
+  value = "https://${azurerm_container_app.this.ingress[0].fqdn}"
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.this.name
+}
+
+output "container_app_environment_id" {
+  value = azurerm_container_app_environment.this.id
+}
+
